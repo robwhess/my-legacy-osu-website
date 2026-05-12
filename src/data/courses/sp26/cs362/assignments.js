a639b607bb7ca4535c6a3661c6870a75ec989f80
Rob Hess <hessro@oregonstate.edu>
2026-05-12 15:41:03 -0700

    Publish CS 362 assignment 4.

--- src/data/courses/sp26/cs362/assignments.js
+++ src/data/courses/sp26/cs362/assignments.js
@@ -43,18 +43,20 @@ const assignments = {
         '[Sign up for a grading demo here](https://outlook.office.com/book/CS362@OregonStateUniversity.onmicrosoft.com/?ismsaljsauthenabled)'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 4 &ndash; End-to-end tests for a complete application',
-    //   due: {
-    //     day: 'Tuesday, 5/28/2024',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Grading demo due by 11:59pm on Friday, 6/14/2024',
-    //     '[Sign up for a grading demo here](https://outlook.office.com/book/CS362@OregonStateUniversity.onmicrosoft.com/?ismsaljsauthenabled)'
-    //   ]
-    // }
+    {
+      link : 'https://classroom.github.com/a/x6ITdvTZ',
+      title: 'Assignment 4 &ndash; End-to-end tests for a complete application',
+      // due: {
+      //   day: 'Tuesday, 5/28/2024',
+      //   time: '11:59pm'
+      // },
+      notes: [
+        'Preferred deadline: **Tuesday, 5/26/2026, 11:59pm**',
+        'Extension deadline: **Thursday, 5/28/2026, 11:59pm** (no submissions after this)',
+        'Grading demo due: **Monday 6/8/2026, 11:59pm**',
+        '[Sign up for a grading demo here](https://outlook.office.com/book/CS362@OregonStateUniversity.onmicrosoft.com/?ismsaljsauthenabled)'
+      ]
+    }
   ],
 
   preamble: 'Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.'
