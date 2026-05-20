62e1374c069d25ccd41e3bd593f1da4fd30b5ae2
Rob Hess <hessro@oregonstate.edu>
2026-05-19 22:07:36 -0700

    Publish CS 493 assignment 4.

--- src/data/courses/sp26/cs493/assignments.js
+++ src/data/courses/sp26/cs493/assignments.js
@@ -43,18 +43,20 @@ const assignments = {
         '[Sign up for a grading demo here](https://outlook.office.com/book/CS493@OregonStateUniversity.onmicrosoft.com/?ismsaljsauthenabled)'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 4 &ndash; File uploads and offline work',
-    //   due: {
-    //     day: 'Monday, 6/3/2024',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Demo due by 11:59pm on Friday, 6/14/2024',
-    //     '[Sign up for a grading demo here](https://outlook.office.com/book/CS493@OregonStateUniversity.onmicrosoft.com/?ismsaljsauthenabled)'
-    //   ]
-    // }
+    {
+      link : 'https://classroom.github.com/a/wAW0DdYu',
+      title: 'Assignment 4 &ndash; File uploads, file storage, and offline work',
+      // due: {
+      //   day: 'Monday, 6/3/2024',
+      //   time: '11:59pm'
+      // },
+      notes: [
+        'Preferred deadline: **Monday, 6/1/2026, 11:59pm**',
+        'Extension deadline: **Wednesday, 6/3/2026, 11:59pm** (no submissions after this)',
+        'Grading demo due: **Friday 6/12/2026, 11:59pm**',
+        '[Sign up for a grading demo here](https://outlook.office.com/book/CS493@OregonStateUniversity.onmicrosoft.com/?ismsaljsauthenabled)'
+      ]
+    }
   ],
 
   preamble: 'Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.'
